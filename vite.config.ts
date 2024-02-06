d3f2628ecaee1dfab2580d915c66b8252efdb091
Кирилл Барабанщиков <lilkirill2020@gmail.com>
2024-02-06 14:35:44 +0400

    add: vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
+
+const PWAOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'PWA Router',
+    short_name: 'PWA Router',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(PWAOptions)],
   resolve: {
     alias: {
       '@': '/src',
-    }
-  }
+    },
+  },
 })
