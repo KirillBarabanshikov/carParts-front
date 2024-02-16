ce17d83d7f11addde2457a78fa8bccdc68941d95
Кирилл Барабанщиков <lilkirill2020@gmail.com>
2024-02-16 20:31:43 +0400

    build: uninstall vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 
-const PWAOptions: Partial<VitePWAOptions> = {
-  registerType: 'autoUpdate',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-  manifest: {
-    name: 'CarParts',
-    short_name: 'CarParts',
-    theme_color: '#f3f6f7',
-    icons: [
-      {
-        src: 'pwa-64x64.png',
-        sizes: '64x64',
-        type: 'image/png',
-      },
-      {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: 'maskable-icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-    ],
-  },
-};
 export default defineConfig({
-  plugins: [react(), VitePWA(PWAOptions)],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
